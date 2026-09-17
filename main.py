6408c1428ac42c5b8a26c3678543024309fa85ae
NotesByVlad <NotesByVlad@gmail.com>
2026-09-17 18:07:55 +0200

    Item class

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 item_1 = "Laptop"
 item_2 = "Coffee"
 
+class Item:
+    def __init__(self, name, sku):
+        self.name = name
+        self.sku = sku
+
+laptop = Item("Gaming Laptop", "LAP01")
+
 warehouse = {
     "Shelf_A": {},
-    "Shelf_B": {}
+    "Shelf_B": {},
+    "Shelf_C": {}
 }
 
 
 def put_away(shelf_name, item, quantity):
 
-    warehouse[shelf_name] = {item, quantity}
+    warehouse[shelf_name] = {item : quantity}
     print("Put away", quantity, "items into", shelf_name)
 
 
 put_away("Shelf_A", item_1, 10)
 put_away("Shelf_B", item_2, 5)
-
+put_away("Shelf_C", laptop, 5)
 
 print("Warehouse:", warehouse)
 
