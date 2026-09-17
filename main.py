cdece2f52bd27425cd0de915ab2bb424415b8927
NotesByVlad <NotesByVlad@gmail.com>
2026-09-17 20:29:09 +0200

    Location class

--- main.py
+++ main.py
@@ -1,31 +1,52 @@
-item_1 = "Laptop"
-item_2 = "Coffee"
+# item_1 = "Laptop"
+# item_2 = "Coffee"
 
 class Item:
     def __init__(self, name, sku):
         self.name = name
         self.sku = sku
 
+    def __repr__(self):
+        return f"{self.name} ({self.sku})"
+
 laptop = Item("Gaming Laptop", "LAP01")
 
-warehouse = {
-    "Shelf_A": {},
-    "Shelf_B": {},
-    "Shelf_C": {}
-}
+class Location:
+    def __init__(self, name):
+        self.name = name
+        self.inventory = {}  
+
+    def add_item(self, item, qty):
+        if item in self.inventory:
+            self.inventory[item] += qty
+        else:
+            self.inventory[item] = qty
+
+    def show_items(self):
+        print(self.inventory)
+        
+shelf_a = Location("shelf_a")
+shelf_a.add_item(laptop, 5)
+shelf_a.show_items() 
+
+# warehouse = {
+#     "Shelf_A": {},
+#     "Shelf_B": {},
+#     "Shelf_C": {}
+# }
+
+# def put_away(shelf_name, item, quantity):
+
+#     warehouse[shelf_name] = {item : quantity}
+#     print("Put away", quantity, "items into", shelf_name)
 
 
-def put_away(shelf_name, item, quantity):
+# put_away("Shelf_A", item_1, 10)
+# put_away("Shelf_B", item_2, 5)
+# put_away("Shelf_C", laptop, 5)
 
-    warehouse[shelf_name] = {item : quantity}
-    print("Put away", quantity, "items into", shelf_name)
 
-
-put_away("Shelf_A", item_1, 10)
-put_away("Shelf_B", item_2, 5)
-put_away("Shelf_C", laptop, 5)
-
-print("Warehouse:", warehouse)
+# print("Warehouse:", warehouse)
 
 
 
