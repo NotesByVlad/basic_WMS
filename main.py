f87d0d0c4b11ca3e3519d26b9fb286a75a6c4ece
NotesByVlad <NotesByVlad@gmail.com>
2026-09-19 09:27:26 +0200

    refactor putaway & prints for check

--- main.py
+++ main.py
@@ -34,33 +34,53 @@ def basic_receiveing(location, item, qty=1):
     location.add_stock(item, qty)
     print(f"Received: \n{item} \nQuantity: {qty}")
 
-def putaway(item, qty, location):
+def putaway(from_location, to_location, item, qty):
+    from_location.remove_stock(item, qty)
+    to_location.add_stock(item, qty)
     print(f"putaway: {item.name} qty: {qty}")
-    location.add_stock(item, qty)
-    print(f"added on {location.name}")
+    print(f"added on {to_location.name}")
 
 def picking(from_location, to_location, item, qty):
     from_location.remove_stock(item, qty)
     to_location.add_stock(item, qty)
+    print(f"picking: {item.name} qty: {qty}")
+    print(f"added to {to_location.name}")
 
 def packing(from_location, to_location, item, qty):
     from_location.remove_stock(item, qty)
     to_location.add_stock(item, qty)
+    print(f"packing: {item.name} qty: {qty}")
+    print(f"added to {to_location.name}")
+    print("Ready for shipment!")
 
 # - - -
 # - -
 # -
 
+def sep():
+    print(20 * "*")
+
 laptop = Item("Gaming Laptop", "LAP01")
 shelf_a = Location("shelf_a")
 
 rec_zone = Location("RECEIVE")
-basic_receiveing(rec_zone, laptop, 7)
-rec_zone.show_stock()
-
 pack_zone = Location("PACKING")
-
 ship_zone = Location("SHIPPING")
 
-# putaway(laptop, 5, shelf_a)
-# shelf_a.show_stock() 
+basic_receiveing(rec_zone, laptop, 7)
+rec_zone.show_stock()
+sep()
+putaway(rec_zone, shelf_a, laptop, 7)
+shelf_a.show_stock()
+sep()
+picking(shelf_a, pack_zone, laptop, 7)
+pack_zone.show_stock()
+sep()
+packing(pack_zone, ship_zone, laptop, 7)
+ship_zone.show_stock()
+sep()
+
+print("Check if remove stock works")
+rec_zone.show_stock()
+shelf_a.show_stock()
+pack_zone.show_stock()
